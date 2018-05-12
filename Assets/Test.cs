e3b013d5ad1f29395894db7a7379ee3bbade76a1
鈴木顕 <yunalovely1977@yahoo.co.jp>
2018-05-12 16:20:27 +0900

    最初のコミット

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 要素数5の配列を初期化する
+		int[] points = new int[5];
+
+		// 配列の各要素に値を代入する
+		points [0] = 30;
+		points [1] = 20;
+		points [2] = 50;
+		points [3] = 10;
+		points [4] = 80;
+
+		// 配列の要素をすべて表示する
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (points [i]);
+		}
+
+		// 配列の要素を逆順に表示する
+		for (int i =4; i >= 0; i--) {
+			Debug.Log (points [i]);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
